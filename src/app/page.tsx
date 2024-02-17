8f4dad0134cc81f090363f448b62a6b3c8b91d2b
Vijayaditya <vijayaditya.eng@gmail.com>
2024-02-17 20:32:41 +0530

    updated home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {turrent_road} from './fonts'
 export default function Home(){
   return (   
     <>
-      <div className={`${turrent_road.className} home h-[85vh] flex justify-center items-center`}>
+      <div className={`${turrent_road.className} home h-[80vh] flex justify-center items-center`}>
         <p className='pb-20'>vijayaditya.source</p>
       </div>
       <div className="relative ">
